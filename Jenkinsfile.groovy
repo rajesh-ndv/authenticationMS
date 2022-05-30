da98ede77eef0b193dbe43e38cdfb59ffb4f9a3c
rajesh-ndv <rajubhai182000@gmail.com>
2022-05-30 22:51:41 +0530

    Removed Commit Message

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -23,7 +23,6 @@ pipeline {
                 withCredentials([usernamePassword(credentialsId: 'eaaa17d0-6623-42da-b0b7-19b7c0949932', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USER_NAME')]) {
                     sh 'git checkout main'
                     sh 'git merge develop'
-                    sh 'git commit -am "Merged develop branch to main"'
                     sh 'git remote remove origin'
                     sh 'git remote add origin https://${GIT_USER_NAME}:${GIT_PASSWORD}@github.com/${GIT_USER_NAME}/authenticationMS.git'
                     sh 'git remote set-url origin https://github.com/rajesh-ndv/authenticationMS.git'
