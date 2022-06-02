89f07bd9c6969f5726eeb102b3989978b6387974
rajesh-ndv <53683997+rajesh-ndv@users.noreply.github.com>
2022-06-02 11:13:00 +0530

    Added PAT Credentials

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -20,9 +20,12 @@ pipeline {
         }
         stage('Deploy'){
             steps{
-                withCredentials([usernamePassword(credentialsId: 'eaaa17d0-6623-42da-b0b7-19b7c0949932', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USER_NAME')]) {
+                withCredentials([gitUsernamePassword(credentialsId: 'Github-PAT', gitToolName: 'Default'), usernamePassword(credentialsId: 'eaaa17d0-6623-42da-b0b7-19b7c0949932', passwordVariable: 'GIT_PAT', usernameVariable: 'GIT_USER')]) {
                     sh 'git checkout main'
                     sh 'git merge develop'
+                    sh 'echo $GIT_USER'
+                    sh 'echo $GIT_PAT'
+                    sh 'git config --local credential.helper "!f() { echo username=\\$GIT_USER; echo password=\\$GIT_PAT; }; f"'
                     sh 'git push origin main'
                 }
             }
